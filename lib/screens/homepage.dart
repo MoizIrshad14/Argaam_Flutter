ff509bdecb7a4538bcb036b57631f568fb010c4d
tahabutt96 <tahabutt1996@gmail.com>
2021-02-11 12:09:37 +0500

    homepage changes

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -23,7 +23,7 @@ class _homepageState extends State<homepage> {
           padding: const EdgeInsets.only(top: 10.0),
           child: Container(
             decoration: BoxDecoration(
-              color: Colors.yellow,
+              color: light_blue_background,
               borderRadius: BorderRadius.vertical(
                 top: Radius.circular(20),
               ),
@@ -32,11 +32,11 @@ class _homepageState extends State<homepage> {
               child: ListView(
                 children: [
                   Padding(
-                    padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                    padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
                     child: Container(
                       height: 110,
                       decoration: BoxDecoration(
-                          color: light_blue_background,
+                          color: dark_blue_background,
                           borderRadius: BorderRadius.all(Radius.circular(12))),
                       //color: Colors.green,
                       child: Row(
@@ -117,89 +117,101 @@ class _homepageState extends State<homepage> {
                     ),
                   ),
                   homeslider(),
+                  SizedBox(height: 8,),
                   ListView.builder(
-                      physics: NeverScrollableScrollPhysics(),
+                    physics: NeverScrollableScrollPhysics(),
                       shrinkWrap: true,
                       itemCount:18,
                       itemBuilder: (context,index){
-                        return Column(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Container(
-                                  decoration: BoxDecoration(
-                                      color: light_blue_background,
-                                      borderRadius: BorderRadius.all(Radius.circular(12))),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [
-                                    Directionality(
-                                      textDirection: TextDirection.rtl,
-                                      child: Column(
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        children: [
-                                          Padding(
-                                            padding: const EdgeInsets.only(right: 10),
-                                            child: Row(
-                                                children: [
-                                                 Text("11/12/2020")
-                                                ]
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 15),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                    decoration: BoxDecoration(
+                                        color: dark_blue_background,
+                                        borderRadius: BorderRadius.all(Radius.circular(12))),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      Directionality(
+                                        textDirection: TextDirection.rtl,
+                                        child: Column(
+                                          mainAxisAlignment: MainAxisAlignment.center,
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            Padding(
+                                              padding: const EdgeInsets.only(right: 10),
+                                              child: Row(
+                                                  children: [
+                                                   Text("11/12/2020")
+                                                  ]
+                                              ),
+                                            ),
+                                            Text("حقق مصرف الراجحي ربحاً صافياً قدره 10.2 ",style: TextStyle(fontSize: 14,fontWeight: FontWeight.bold),),
+                                            Text("مليار ريال للسنة المالية 2019",style: TextStyle(fontSize: 14,fontWeight: FontWeight.bold),),
+                                            SizedBox(height: 10),
+                                            Row(
+                                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                              children: [
+                                                Row(
+                                                  children: [
+                                                    Container(
+                                                        padding: EdgeInsets.only(left:5),
+                                                        child: Icon(Icons.access_time)),
+                                                    Text("قبل 45 دقيقة",style: TextStyle(fontSize: 14,fontWeight: FontWeight.bold),)
+                                                  ],
+                                                ),
+                                                Container(
+                                                  padding: EdgeInsets.only(right: 50),
+                                                  child: Row(
+                                                    children: [
+                                                      Text("45",style: TextStyle(fontSize: 14,fontWeight: FontWeight.bold),),
+                                                      Container(
+                                                          padding: EdgeInsets.only(right:5),
+                                                          child: Icon(Icons.messenger_outline)),
+                                                      Container(
+                                                          padding: EdgeInsets.only(right:5),
+                                                          child: Icon(Icons.favorite_outline)),
+                                                      Container(
+                                                          padding: EdgeInsets.only(right:5,left: 10),
+                                                          child: Icon(Icons.arrow_upward)),
+                                                    ],
+                                                  ),
+                                                )
+                                              ],
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                      Container(
+                                        margin: EdgeInsets.all(15),
+                                        height: 100,
+                                        width: 100,
+                                          decoration: BoxDecoration(
+                                            borderRadius: BorderRadius.circular(8.0),
+                                            image: DecorationImage(
+                                              image: AssetImage("assets/images/blog.png"),
+                                              fit: BoxFit.cover,
                                             ),
                                           ),
-                                          Text("حقق مصرف الراجحي ربحاً صافياً قدره 10.2 ",style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),),
-                                          Text("مليار ريال للسنة المالية 2019",style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),),
-                                          SizedBox(height: 10),
-                                          Row(
-                                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                            children: [
-                                              Row(
-                                                children: [
-                                                  Container(
-                                                      padding: EdgeInsets.only(left:5),
-                                                      child: Icon(Icons.access_time)),
-                                                  Text("قبل 45 دقيقة",style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),)
-                                                ],
-                                              ),
-                                              Row(
-                                                children: [
-                                                  Text("45",style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),),
-                                                  Container(
-                                                      padding: EdgeInsets.only(right:5),
-                                                      child: Icon(Icons.messenger_outline)),
-                                                  Container(
-                                                      padding: EdgeInsets.only(right:5),
-                                                      child: Icon(Icons.favorite_outline)),
-                                                  Container(
-                                                      padding: EdgeInsets.only(right:5,left: 10),
-                                                      child: Icon(Icons.arrow_upward)),
-                                                ],
-                                              )
-                                            ],
-                                          )
-                                        ],
                                       ),
-                                    ),
-                                    Container(
-                                      height: 140,
-                                      width: 150,
-                                      color: Colors.black,
-                                    ),
-                                  ],
-                                )
+                                    ],
+                                  )
+                                ),
                               ),
-                            ),
-                            SizedBox(height: 10)
-                          ],
+                              SizedBox(height: 10)
+                            ],
+                          ),
                         );
                       })
                 ],
               ),
             ),
-            // child: Padding(
-            //   padding: const EdgeInsets.all(25.0),
-            //   child: hometabs(),
-            // ),
           ),
         ),
       ),
